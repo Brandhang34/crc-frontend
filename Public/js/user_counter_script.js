9982487006fc5abb58b10f6a1d8a4c3fb3f0ca4e
brandon <brandonhang34@outlook.com>
2023-02-03 22:11:15 -0800

    formatted user counter script

--- Public/js/user_counter_script.js
+++ Public/js/user_counter_script.js
@@ -1,6 +1,3 @@
-// Initializes data to the default dictionary
-let data = { user: "visitor" };
-
 // fetches the api created in API Gateway and uses the post method to write to
 // the dynamodb table and retrieve the amount of users who have triggered this api
 
@@ -8,14 +5,9 @@ const counterElement = document.getElementById("counter");
 
 fetch("https://16un4jjquc.execute-api.us-east-1.amazonaws.com/prod/count", {
   method: "POST",
-  headers: { "Content-Type": "application/json" },
-  body: JSON.stringify(data),
 })
   .then((res) => res.json())
   .then((data) => {
-    console.log(data["message"]);
-    const counter = document.createElement("p");
     counter.innerText = data["message"];
-    counterElement.append(counter);
   })
   .catch((error) => console.log(error));
